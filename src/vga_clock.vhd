b42b0c1eefef2de5a263c050022190e335ebc1b0
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-04-18 14:54:41 +1200

    Folder Rearrangement

---
+++ src/vga_clock.vhd
@@ -0,0 +1,15 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE  IEEE.STD_LOGIC_ARITH.all;
+USE  IEEE.STD_LOGIC_UNSIGNED.all;
+
+entity vga_clock is
+	port(Clk : in std_logic;
+	vgaClock : out std_logic);
+end entity;
+
+architecture bhv of vga_clock is
+
+begin
+
+end architecture bhv;

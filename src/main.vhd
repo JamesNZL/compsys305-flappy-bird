5a0a9fcf51820c1082317854f1fa31ae1f8fbb97
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-04-29 18:51:14 +1200

    Working bird jump and obstacle scroll

--- src/main.vhd
+++ src/main.vhd
@@ -18,6 +18,7 @@
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all; 
+use ieee.numeric_std.all;
 
 LIBRARY work;
 
@@ -50,21 +51,26 @@ COMPONENT vga_sync
 		 blue_out : OUT STD_LOGIC;
 		 horiz_sync_out : OUT STD_LOGIC;
 		 vert_sync_out : OUT STD_LOGIC;
-		 pixel_column : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
-		 pixel_row : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
+		 pixel_column : OUT signed(9 DOWNTO 0);
+		 pixel_row : OUT signed(9 DOWNTO 0)
 	);
 END COMPONENT;
+
+component obstacle is
+   port(
+        enable, pb1, clk, vert_sync : in std_logic;
+        pixel_row, pixel_column : in signed(9 downto 0);
+        green, inPixel : out std_logic);--red, green, blue, inPixel : out std_logic);
+end component;
 
 component MOUSE
    PORT( clock_25Mhz, reset 		: IN std_logic;
          mouse_data					: INOUT std_logic;
          mouse_clk 					: INOUT std_logic;
          left_button, right_button	: OUT std_logic;
-		   mouse_cursor_row 			: OUT std_logic_vector(9 DOWNTO 0); 
-		   mouse_cursor_column 		: OUT std_logic_vector(9 DOWNTO 0));       	
+		   mouse_cursor_row 			: OUT signed(9 DOWNTO 0); 
+		   mouse_cursor_column 		: OUT signed(9 DOWNTO 0));       	
 END component;
-
-
 
 COMPONENT pll
 	PORT(refclk : IN STD_LOGIC;
@@ -74,32 +80,34 @@ COMPONENT pll
 	);
 END COMPONENT;
 
-COMPONENT bouncy_ball
-	PORT(pb1 : IN STD_LOGIC;
-		 pb2 : IN STD_LOGIC;
-		 clk : IN STD_LOGIC;
-		 vert_sync : IN STD_LOGIC;
-		 pixel_column : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-		 pixel_row : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-		 red : OUT STD_LOGIC;
-		 green : OUT STD_LOGIC;
-		 blue : OUT STD_LOGIC
-	);
+COMPONENT bird
+	PORT(pb1, pb2, clk, vert_sync : in std_logic;
+        pixel_row, pixel_column : in signed(9 downto 0);
+        red, inPixel : out std_logic);--green, blue, inPixel : out std_logic);
 END COMPONENT;
 
 SIGNAL	vgaClk :  STD_LOGIC;
 SIGNAL	paintR :  STD_LOGIC;
 SIGNAL	paintG :  STD_LOGIC;
 SIGNAL	paintB :  STD_LOGIC;
+SIGNAL	birdR :  STD_LOGIC;
+SIGNAL	birdG :  STD_LOGIC;
+SIGNAL	birdB :  STD_LOGIC;
+SIGNAL	obsR :  STD_LOGIC;
+SIGNAL	obsG :  STD_LOGIC;
+SIGNAL	obsB :  STD_LOGIC;
 SIGNAL	Reset :  STD_LOGIC;
 SIGNAL	VSYNC :  STD_LOGIC;
-SIGNAL	xPos :  STD_LOGIC_VECTOR(9 DOWNTO 0);
-SIGNAL	yPos :  STD_LOGIC_VECTOR(9 DOWNTO 0);
+SIGNAL	xPos :  signed(9 DOWNTO 0);
+SIGNAL	yPos :  signed(9 DOWNTO 0);
 SIGNAL	LEFTBUTTONevent :  STD_LOGIC;
 SIGNAL	RIGHTBUTTONevent :  STD_LOGIC;
-SIGNAL	MOUSEROW :  STD_LOGIC_VECTOR(9 DOWNTO 0);
-SIGNAL	MOUSECOLUMN :  STD_LOGIC_VECTOR(9 DOWNTO 0);
- 
+SIGNAL	MOUSEROW :  signed(9 DOWNTO 0);
+SIGNAL	MOUSECOLUMN :  signed(9 DOWNTO 0);
+SIGNAL	OBST1 : std_logic;
+SIGNAL	ObDet : std_logic;
+SIGNAL	BiDet : std_logic;
+
 
 
 BEGIN 
@@ -131,7 +139,16 @@ PORT MAP(clock_25Mhz => vgaClk,
 		   mouse_cursor_row => MOUSEROW,
 		   mouse_cursor_column => MOUSECOLUMN);
 
-
+obstacle1 : obstacle
+port map(enable => OBST1,
+			pb1 => pb1,
+			clk => vgaClk,
+			vert_sync => VSYNC,
+         pixel_row => xPos,
+			pixel_column => yPos,
+         --red => obsR,
+			green => paintG);--obsG),
+			--blue => obsB);
 
 b2v_inst3 : pll
 PORT MAP(refclk => clk,
@@ -139,16 +156,46 @@ PORT MAP(refclk => clk,
 		 outclk_0 => vgaClk);
 
 
-b2v_inst5 : bouncy_ball
+b2v_inst5 : bird
 PORT MAP(pb1 => pb1,
 		 pb2 => LEFTBUTTONevent,
 		 clk => vgaClk,
 		 vert_sync => VSYNC,
 		 pixel_column => xPos,
 		 pixel_row => yPos,
-		 red => paintR,
-		 green => paintG,
-		 blue => paintB);
+		 red => paintR); --birdR,
+		 --green => birdG,
+		 --blue => birdB);
 
+--SET TEST OBSTACLE ENABLE		 
+OBST1 <= '1';
+
+setObstacles : process (clk)
+begin
+	if rising_edge(clk) then
+		--SET OBSTACLES INTERMITTENTLY
+	end if;
+end process setObstacles;
+
+-------------DRAWING--------------
+
+drawSprite : process (clk)
+begin
+	if rising_edge(clk) then
+		
+		if (BiDet = '1') then
+			paintR <= birdR;
+			paintG <= birdG;
+			paintB <= birdB;
+		elsif (ObDet = '1') then
+			paintR <= obsR;
+			paintG <= obsG;
+			paintB <= obsB;
+		end if;
+		
+	end if;
+end process drawSprite;
+
+----------------------------------
 
 END bdf_type;
---
+++ src/obstacle.vhd
@@ -0,0 +1,52 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use ieee.numeric_std.all;
+
+entity obstacle is
+    port (
+        enable, pb1, clk, vert_sync : in std_logic;
+        pixel_row, pixel_column : in signed(9 downto 0);
+        green, inPixel : out std_logic);--red, green, blue, inPixel : out std_logic);
+end obstacle;
+
+architecture behavior of obstacle is
+
+    signal obstacle_on : std_logic;
+    signal gapSize : signed(9 downto 0);
+	 signal pipeWidth : signed(9 downto 0);
+    signal gapCenter : signed(9 downto 0);
+    signal xPos : signed(10 downto 0) := TO_SIGNED(640,11);
+    signal xVelocity : signed(9 downto 0) := TO_SIGNED(5,10);
+	 signal reset : std_logic;
+	 signal drawObstacle : std_logic;
+
+begin
+
+    gapSize <= TO_SIGNED(25,10);
+	 gapCenter <= TO_SIGNED(280,10);
+	 pipeWidth <= TO_SIGNED(25, 10);
+	
+	 drawObstacle <= '1' when (('0' & xPos <= '0' & pixel_row + pipeWidth) and ('0' & pixel_row <= '0' & xPos + pipeWidth)
+               and (('0' & gapCenter >= pixel_column + gapSize) or ('0' & pixel_column >= gapCenter + gapSize))) else
+               '0';
+
+    Green <= drawObstacle;
+	 
+    move_obstacle : process (vert_sync)
+    begin
+		if (rising_edge(vert_sync)) then
+		
+			if (reset = '0') then
+				xPos <= xPos - xVelocity;
+			else
+				xPos <= TO_SIGNED(640,11);
+			end if;
+		
+		end if;
+    end process move_obstacle;
+	 
+	 reset <= '1' when (pb1 = '0') else '0';
+	 
+	 inPixel <= drawObstacle;
+
+end behavior;

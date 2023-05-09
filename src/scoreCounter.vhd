ad60b4f6d4dc925db657f1523be09374887195c2
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-05-09 14:33:22 +1200

    Finished Score Counter, Needs char rom.

--- src/scoreCounter.vhd
+++ src/scoreCounter.vhd
@@ -21,18 +21,18 @@ begin
  begin
   if rising_edge(Clk) then
 
-    if ((Tick = '1') and (flag = '0')) then
+    if (((Tick = '1') and (flag = '0')) or (Reset = '0')) then
         flag <= '1';
         if (Reset = '0') then
             t_Q := "0000";
-        elsif (t_Q /= "1010") then
+        elsif (t_Q /= "1001") then
             setNextDigit <= '0';
             t_Q := t_Q + 1;
         else
             setNextDigit <= '1';
             t_Q := "0000";
         end if;
-    else
+    elsif (Tick = '0') then
         flag <= '0';
     end if;
 
